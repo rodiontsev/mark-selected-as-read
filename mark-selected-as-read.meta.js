d944f1210a6daf7cfe71323370e264165919d5f9
Dmitry Rodiontsev <dmitry.rodiontsev@gmail.com>
2012-01-18 09:22:17 -0500

    Add mark-selected-as-read.meta.js

---
+++ mark-selected-as-read.meta.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name          Google Reader - Mark Selected Items as Read
+// @version       1.1.1
+// @namespace     http://twitter.com/dimarad
+// @description   This script adds the button "Mark selected as read" and a checkbox for an each item
+// @author        Dmitry Rodiontsev
+// @include       htt*://www.google.tld/reader/view/*
+// ==/UserScript==
