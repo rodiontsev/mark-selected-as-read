09fa16a486966b65ded5764247e36073f577114b
Dmitry Rodiontsev <dmitry.rodiontsev@gmail.com>
2012-06-18 13:15:24 +0400

    Update links on my twitter account

--- mark-selected-as-read.user.js
+++ mark-selected-as-read.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          Google Reader - Mark Selected Items as Read
 // @version       1.1.1
-// @namespace     http://twitter.com/dimarad
+// @namespace     http://twitter.com/rodiontsev
 // @description   This script adds the button "Mark selected as read" and a checkbox for an each item
 // @author        Dmitry Rodiontsev
 // @include       htt*://www.google.tld/reader/view/*
